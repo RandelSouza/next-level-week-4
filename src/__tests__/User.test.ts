1f6ad7d7f6998d014fabdc07723ab5fe8390ea83
randelsouza <randelsouza88@gmail.com>
2021-02-27 16:43:55 -0300

    Added User.test.ts file

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+import { app } from '../app';
+
+
+describe("Users", () => {
+    request(app)
+        .post("/users")
+        .send({
+            name: "User Example",
+            email: "user@example.com"            
+        }
+    );
+});
