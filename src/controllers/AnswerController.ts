372c17c3ef98b08d78f6a4c223a342e50df56e0f
randelsouza <randelsouza88@gmail.com>
2021-02-28 17:34:04 -0300

    Added functionality value for survey user

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 
 class AnswerController {
-    async execute(request: Request, response: Response){
-        
-    }
 
-}
+    // http://localhost:3333/answers/0?u=41503a2d-8414-42cc-9e4f-e03a88adf507
+
+    async execute(request: Request, response: Response){
+        const { value } = request.params;
+        const { u } = request.query;
+        
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u)
+        });
+
+        if(!surveyUser){
+            return response.status(400).json({
+                message_error: "Survey User does not exists!"
+            });
+        };
+        
+        surveyUser.value = Number(value);
+
+        await surveysUsersRepository.save(surveyUser);
+
+        response.json(surveyUser);
+    };
+};
 
 export { AnswerController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { AnswerController } from './controllers/AnswerController';
 import { SendMailController } from './controllers/SendMailController';
 import { SurveyController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
@@ -7,10 +8,12 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendEmailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", userController.create);
 router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 router.post("/sendMail", sendEmailController.execute);
+router.get("/answers/:value", answerController.execute);
 
 export { router };
