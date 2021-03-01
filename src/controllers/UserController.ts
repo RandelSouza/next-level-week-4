ae0ee2e3365e39a994b56a13b376444bd1ea199b
randelsouza <randelsouza88@gmail.com>
2021-03-01 09:36:44 -0300

    Added Validation with yup

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,33 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
 import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
     async create(request: Request, response: Response){
         const { name, email } = request.body;
         
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+
+        });
+
+        // if(!(await schema.isValid(request.body))){
+        //     return response.status(400).json({
+        //         message_error: "Validation Failed!"
+        //     });
+        // }
+
+        try{
+            
+            await schema.validate(request.body, { abortEarly: false });    
+
+        }catch(error){
+            return response.status(400).json(error);
+        }
+        
+
         const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne( { email } );
