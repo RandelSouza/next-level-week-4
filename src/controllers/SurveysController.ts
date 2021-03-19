d603e89af03afbf38f8af7cd8bfe9c1c7320b17d
randelsouza <randelsouza88@gmail.com>
2021-03-19 11:28:45 -0300

    Moodified SurveysController.ts remove constructor

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -2,11 +2,7 @@ import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 
-class SurveyController {
-
-    constructor(){
-        console.log("teste");        
-    }
+class SurveyController {    
 
     async create(request: Request, response: Response) {
         const { title, description } = request.body;
